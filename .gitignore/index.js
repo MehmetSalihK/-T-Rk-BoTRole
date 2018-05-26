f067a245a65512265af222bddc8bd722b79d5bcc
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-05-26 16:39:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,23 +1,52 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-const TOKEN = "NDQ1NjQxMTU5MjM1MzM4MjQw.DdtbRA.O8V8ZLlhFnPBUJf7fO7zH818qT4";
-const ownerID = "174565176492687361"
-const PREFIX = "-";
-
-var servers = {};
-
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "genel").send(`**Bienvenue à** __**${member.guild.name}**__ **maintenant** __**${member.user.username}**__ **est avec nous!**`)
-})
-
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "genel").send(`__**${member.user.username}**__ **n'est plus sur notre serveur** __**${member.guild.name}**__`)
-})
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find("name", "👤YENI👤");
-    member.addRole(role)
-})
+    let channel = member.guild.channels.find('name', 'admin-messajı');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Sunucumuza katılan kişi : ', `${member}`)
+        .addField(':microphone2: | Hoşgeldiniz!', `Sunucuya hoşgeldin, ${member}`)
+        .addField(':id: | Kullanıcı :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | Sunucumuzda ', `${member.guild.memberCount}` + "cu sıradasınız")
+        .addField("Isim", `<@` + `${member.id}` + `>`, true)
+        .addField('Sunucu', `${member.guild.name}`, true )
+        .setFooter("Yardım için [-yardim]", "https://imgur.com/w0z6lI3.png")
+        .setTimestamp()
 
-bot.login(TOKEN);
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "katıldı" + `${member.guild.name}`)
+
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'admin-messajı');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Isim:', `${member}`)
+        .addField('Neden ?', 'Hayırdır Laaaan kim uzdu seni')
+        .addField('Bye Bye :(', 'Yolun açık olsun kardeşim!')
+        .addField('Sunucuda şimdi', `${member.guild.memberCount}` + "kişi var")
+        .setFooter("Yardım için [-yardim]", "https://imgur.com/w0z6lI3.png")
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    console.log(`${member}` + "ayrıldı" + `${member.guild.name}` + "Şimdi mesaj bırakın")
+    console.log("Mesajın Gönderildi")
+});
+
+const token = process.env.TOKEN;
+bot.login("NDQ1NjQxMTU5MjM1MzM4MjQw.DesDJw.b2UYAgxgpBst3amFvkfE4nGHFns")
