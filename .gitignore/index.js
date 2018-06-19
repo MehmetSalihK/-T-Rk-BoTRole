84bf4519634e2b8fec42be0bb5b3732243180426
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-19 08:10:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,6 +21,25 @@ bot.on('guildMemberAdd', member => {
 });
 
 bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '💬sohbet');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Sunucumuza katılan kişi : ', `${member}`)
+        .addField(':microphone2: | Hoşgeldiniz!', `Sunucuya hoşgeldin, ${member}`)
+        .addField(':id: | Kullanıcı :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | Sunucumuzda ', `${member.guild.memberCount}` + "cu sıradasınız")
+        .addField("Isim", `<@` + `${member.id}` + `>`, true)
+        .addField('Sunucu', `${member.guild.name}`, true )
+        .setFooter("Yardım için [-yardim]", "https://imgur.com/w0z6lI3.png")
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberAdd', member => {
 
     console.log(`${member}`, "katıldı" + `${member.guild.name}`)
 
@@ -28,6 +47,23 @@ bot.on('guildMemberAdd', member => {
 
 bot.on('guildMemberRemove', member => {
     let channel = member.guild.channels.find('name', 'admin-messajı');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Isim:', `${member}`)
+        .addField('Neden ?', 'Hayırdır Laaaan kim uzdu seni')
+        .addField('Bye Bye :(', 'Yolun açık olsun kardeşim!')
+        .addField('Sunucuda şimdi', `${member.guild.memberCount}` + "kişi var")
+        .setFooter("Yardım için [-yardim]", "https://imgur.com/w0z6lI3.png")
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', '💬sohbet');
     let memberavatar = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
